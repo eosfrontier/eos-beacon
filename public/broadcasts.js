fbf79c8ef4a761b77202fb450baa04b48d9e9bbf
WafflesMcDuff <goldjd@gmail.com>
2026-01-25 18:04:03 +0100

    Sort files into folders

--- public/broadcasts.js
+++ public/broadcasts.js
@@ -23,25 +23,27 @@ var bcdefault = new broadcastObj("Standby", "bcdefault", 1, "0", "0");
 var bcreset = new broadcastObj("Standby", "bcdefault", 99, "0", "0");
 
 /* kitchen crew */
-var bclunch = new broadcastObj("Lunch announcement", "bclunch", 2, "0", "0");
-var bcdinner = new broadcastObj("Dinner announcement", "bcdinner", 2, "0", "0");
-var bcdishes = new broadcastObj("Dishes reminder", "bcdishes", 2, "0", "0");
+var bclunch = new broadcastObj("Lunch announcement", "kitchen/bclunch", 2, "0", "0");
+var bcdinner = new broadcastObj("Dinner announcement", "kitchen/bcdinner", 2, "0", "0");
+var bcdishes = new broadcastObj("Dishes reminder", "kitchen/bctbcdishes", 2, "0", "0");
 
 /* other PSA's */
 var bctempleservice = new broadcastObj("Tachar Service", "bctempleservice", 2, "600000", "0");
-var bcmedical = new broadcastObj("Blood Donation Request", "bcmedical", 2, "0", "0");
 var bcmeeting = new broadcastObj("Meeting in Main", "bcmeeting", 4, "0", "0");
-var bcblood = new broadcastObj("Blood Donation Demand", "bcblood", 4, "0", "0");
-var bcmedbay = new broadcastObj("Medical Personel to Medbay", "bcmedbay", 6, "0", "0");
-var bcmedicalsupport = new broadcastObj("Insufficient Medical Staff", "bcmedicalsupport", 6, "0", "0");
+
+/* medical PSA's */
+var bcmedical = new broadcastObj("Blood Donation Request", "medical/bcmedical", 2, "0", "0");
+var bcblood = new broadcastObj("Blood Donation Demand", "medical/bcblood", 4, "0", "0");
+var bcmedbay = new broadcastObj("Medical Personel to Medbay", "medical/bcmedbay", 6, "0", "0");
+var bcmedicalsupport = new broadcastObj("Insufficient Medical Staff", "medical/bcmedicalsupport", 6, "0", "0");
 
 /* reminders */
-var bchydrate = new broadcastObj("Hydration Reminder", "bchydrate", 1, "600000", "0");
-var bcsunscreen = new broadcastObj("Sunscreen Reminder", "bcsunscreen", 1, "600000", "0");
-var bcthankyou = new broadcastObj("Kindness Reminder", "bcthankyou", 1, "600000", "0");
-var bctalon = new broadcastObj("Talon Reminder", "bctalon", 1, "600000", "0");
-var bcmorning = new broadcastObj("Morning Reminder", "bcmorning", 1, "3600000", "0");
-var bcgotobed = new broadcastObj("Sleep Reminder", "bcgotobed", 1, "10800000", "gray");
+var bchydrate = new broadcastObj("Hydration Reminder", "reminders/bchydrate", 1, "600000", "0");
+var bcsunscreen = new broadcastObj("Sunscreen Reminder", "reminders/bcsunscreen", 1, "600000", "0");
+var bcthankyou = new broadcastObj("Kindness Reminder", "reminders/bcthankyou", 1, "600000", "0");
+var bctalon = new broadcastObj("Talon Reminder", "reminders/bctalon", 1, "600000", "0");
+var bcmorning = new broadcastObj("Morning Reminder", "reminders/bcmorning", 1, "3600000", "0");
+var bcgotobed = new broadcastObj("Sleep Reminder", "reminders/bcgotobed", 1, "10800000", "gray");
 
 /* hazards */
 var bcviral = new broadcastObj("Unknown Viral Pathogens detected", "bcviral", 8, "0", "hazard");
@@ -58,26 +60,28 @@ var bchostileoutside = new broadcastObj("Enemy Contact", "bchostileoutside", 8, 
 var bchostileinside = new broadcastObj("Enemy Contact", "bchostileinside", 8, "0", "attack");
 
 /* portal */
-var bcportalinc = new broadcastObj("Scheduled Incoming Portal Activation", "bcportalinc", 3, "30000", "0");
-var bcportalincdanger = new broadcastObj("Unscheduled Incoming Portal Activation", "bcportalincdanger", 3, "30000", "0");
-var bcportalout = new broadcastObj("Portal Outgoing", "bcportalout", 3, "17500", "0");
+var bcportalinc = new broadcastObj("Scheduled Incoming Portal Activation", "portal/bcportalinc", 3, "30000", "0");
+var bcportalincdanger = new broadcastObj("Unscheduled Incoming Portal Activation", "portal/bcportalincdanger", 3, "30000", "0");
+var bcportalout = new broadcastObj("Portal Outgoing", "portal/bcportalout", 3, "17500", "0");
 
-var bcportalinc_voice = new broadcastObj("Scheduled Incoming Portal Activation", "bcportalinc_voice", 3, "30000", "0");
-var bcportalincdanger_voice = new broadcastObj("Unscheduled Incoming Portal Activation", "bcportalincdanger_voice", 3, "30000", "0");
-var bcportalout_voice = new broadcastObj("Portal Outgoing", "bcportalout_voice", 3, "17500", "0");
+var bcportalinc_voice = new broadcastObj("Scheduled Incoming Portal Activation", "portal/bcportalinc_voice", 3, "30000", "0");
+var bcportalincdanger_voice = new broadcastObj("Unscheduled Incoming Portal Activation", "portal/bcportalincdanger_voice", 3, "30000", "0");
+var bcportalout_voice = new broadcastObj("Portal Outgoing", "portal/bcportalout_voice", 3, "17500", "0");
 
 /* Overlord-only */
-var bctransmission = new broadcastObj("Incoming Transmission", "bctransmission", 5, "0", "0");
-var bctransmissionend = new broadcastObj("Incoming Transmission", "bctransmissionend", 10, "17500", "0");
-var bclowpower = new broadcastObj("POWER SUPPLY WARNING", "bclowpower", 8, "0", "gray");
-var bcreactorcrit = new broadcastObj("Reactor Critical", "bcreactorcrit", 9, "0", "attack");
-var bchackattack = new broadcastObj("Security Breach", "bchackattack", 9, "0", "0");
+var bctransmission = new broadcastObj("Incoming Transmission", "overlord/bctransmission", 5, "0", "0");
+var bctransmissionend = new broadcastObj("Incoming Transmission", "overlord/bctransmissionend", 10, "17500", "0");
+var bclowpower = new broadcastObj("POWER SUPPLY WARNING", "overlord/bclowpower", 8, "0", "gray");
+var bcreactorcrit = new broadcastObj("Reactor Critical", "overlord/bcreactorcrit", 9, "0", "attack");
+var bchackattack = new broadcastObj("Security Breach", "overlord/bchackattack", 9, "0", "0");
+var bcmissionout = new broadcastObj("Strati.OS Mission Alert", "overlord/bcMissionout", 9, "0", "0");
+
 
 /* Maati's Eos IT toolbox */
 var bcsystemscan = new broadcastObj("System Diagnostics", "bcsystemscan", 88, "30000", "0");
 var bcfreezeall = new broadcastObj("ADMIN.OVERRIDE", "bcfreezeall", 100, "0", "0");
 
 /* VIDEOS */
-var bcspoilervideo = new broadcastObj("VID.TRANSMISSION","bcspoilervideo",9,"0","0");
-var bcvoti = new broadcastObj("VID.TRANSMISSION","bcvoti",9,"0","0");
-var bczorski = new broadcastObj("VID.TRANSMISSION","bczorski",9,"0","0");
+var bcspoilervideo = new broadcastObj("VID.TRANSMISSION","videos/bcspoilervideo",9,"0","0");
+var bcvoti = new broadcastObj("VID.TRANSMISSION","videos/bcvoti",9,"0","0");
+var bczorski = new broadcastObj("VID.TRANSMISSION","videos/bczorski",9,"0","0");
