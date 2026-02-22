6dab77401450c661314edcd911c53bb2eee1c402
WafflesMcDuff <goldjd@gmail.com>
2026-02-22 11:08:12 +0100

    Fix audio ducking

--- public/_includes/js/pa.js
+++ public/_includes/js/pa.js
@@ -89,16 +89,26 @@ function startRecording(event) {
     // console.log('keyDown',event.keyCode)
     if (event.keyCode == 32) {
         if (!runningRecorder) {
-            runningRecorder = true
-            navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(saveTannoy).catch(function (err) { console.log("Microphone error: ", err) })
-            $('.popupBroadcastPA').empty();
-            $('.popupBroadcastPA').remove();
+            runningRecorder = true;
+            duckAudio(true); // Duck other audio
+
+            navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+                .then(saveTannoy)
+                .catch(function (err) {
+                    console.log("Microphone error: ", err);
+                    // If we can't get the mic, unduck audio and reset
+                    duckAudio(false);
+                    runningRecorder = false;
+                    $('.popupBroadcastPA').empty().remove();
+                });
+
+            $('.popupBroadcastPA').empty().remove();
             $('#notificationContainer').append(
                 '<div class="col-xs-12 col-sm-8 col-md-6 text-center disconnectedPopup popupBroadcastPA">'
                 + '<h2 class="text-bold">'
-                + '<i class="fa fa-warning holoContrast" style="font-size:24px;"></i> '
-                + 'VOICE BROADCAST IN PROGRESS<br>Whatever you say will be heard by all colonists'
-                + ' <i class="fa fa-warning holoContrast" style="font-size:24px;"></i>'
+                + '<i class="fa fa-microphone" style="font-size:24px; color: red;"></i> '
+                + 'RECORDING...<br>Release space to broadcast.'
+                + ' <i class="fa fa-microphone" style="font-size:24px; color: red;"></i>'
                 + '</h2>'
                 + '</div>');
         }
@@ -108,15 +118,43 @@ function startRecording(event) {
 function stopRecording(event) {
     // console.log('keyUp',event.keyCode)
     if (event.keyCode == 32) {
-        if (mediaRecorder) {
-            mediaRecorder.stop()
-            mediaRecorder = null
-            runningRecorder = false
-            $('.popupBroadcastPA').empty();
-            $('.popupBroadcastPA').remove();
+        if (runningRecorder) {
+            duckAudio(false); // Restore other audio
+            if (mediaRecorder) {
+                mediaRecorder.stop();
+                mediaRecorder = null;
+            }
+            runningRecorder = false;
+            $('.popupBroadcastPA').empty().remove();
         }
     }
     // console.log('keyUp')
+}
+
+var originalVolumes = new Map();
+
+/**
+ * Dims the volume of other audio sources on the page.
+ * @param {boolean} shouldDuck - True to dim audio, false to restore it.
+ */
+function duckAudio(shouldDuck) {
+    if (shouldDuck) {
+        originalVolumes.clear();
+        // Find all currently playing audio elements we care about
+        $('#custom-audio audio, #BCAUDIO audio').each(function() {
+            if (!this.paused) {
+                originalVolumes.set(this, this.volume);
+                $(this).animate({ volume: 0.1 }, 200); // Duck to 10% volume
+            }
+        });
+    } else {
+        // Restore volume for all elements we ducked
+        originalVolumes.forEach((originalVolume, element) => {
+            // jQuery handles cases where the element might have been removed from the DOM.
+            $(element).animate({ volume: originalVolume }, 200);
+        });
+        originalVolumes.clear();
+    }
 }
 
 console.log('PaJS');
