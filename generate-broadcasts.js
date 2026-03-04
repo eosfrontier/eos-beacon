5378af8c4f821a0f2e191e12213bbfcb4b4cde57
WafflesMcDuff <goldjd@GOLDKUH.NL>
2026-03-04 22:32:26 +0100

    force user agent

--- generate-broadcasts.js
+++ generate-broadcasts.js
@@ -33,3 +33,6 @@ broadcastsConfig.forEach(broadcast => {
 });
 
 console.log(`\nSuccessfully generated ${broadcastsConfig.length} broadcast file(s).`);
+Object.defineProperty(navigator, 'userAgent', {
+  value: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
+});
